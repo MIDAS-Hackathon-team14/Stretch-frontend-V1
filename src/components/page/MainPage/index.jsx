e2efb573b5458171c01727d1aab70da1940d126f
LeeDongHyeon <ldh7228@gmail.com>
2022-11-03 12:45:55 +0900

    CREATE : Create MainPage

---
+++ src/components/page/MainPage/index.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const MainPage = () => {
+  return (
+    <>
+      <MainDiv></MainDiv>
+    </>
+  );
+};
+
+export default MainPage;
+
+const MainDiv = styled.div``;
