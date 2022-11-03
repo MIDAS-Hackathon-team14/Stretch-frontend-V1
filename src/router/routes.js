c9d61ed06ba8a73e6429479bdfacb73d35e6a6c3
LeeDongHyeon <ldh7228@gmail.com>
2022-11-04 01:47:32 +0900

    ADD : Add EnterCode

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,6 +3,8 @@ import SignUp from "../components/page/Auth/SignUp";
 import MainPage from "../components/page/MainPage";
 import SignIn from "../components/page/Auth/SignIn";
 import Select from "../components/page/Auth/Select";
+import InviteCode from "../components/page/Auth/invite";
+import EnterCode from "../components/page/Auth/EnCode";
 
 const Router = () => {
   return (
@@ -13,6 +15,8 @@ const Router = () => {
           <Route path="/signup" element={<SignUp />}></Route>
           <Route path="/login" element={<SignIn />}></Route>
           <Route path="/select" element={<Select />}></Route>
+          <Route path="/invite" element={<InviteCode />}></Route>
+          <Route path="/encode" element={<EnterCode />}></Route>
         </Routes>
       </BrowserRouter>
     </>
