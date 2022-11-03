aeef0b57b4201d1e0d3f267a8f936a0650d635d1
LeeDongHyeon <ldh7228@gmail.com>
2022-11-04 06:42:45 +0900

    Update : Update Side bar

--- src/components/common/Sidebar/index.jsx
+++ src/components/common/Sidebar/index.jsx
@@ -1,25 +1,90 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { BASE_URL, EmployList } from "../../../lib/export/data";
+import CompanyLogo from "../../../asset/image/companyLogo.png";
+import { useDispatch, useSelector } from "react-redux";
+import { settingPlan } from "../../../store/setPlan";
+import { Link } from "react-router-dom";
 
 const SideBar = () => {
+  const dispatch = useDispatch();
+  const plan = useSelector((state) => state.plan.setPlan);
+  const update = useSelector((state) => state.plan.setUpdate);
+
+  const admin = true;
+  const [company, setCompany] = useState({
+    company_id: 1,
+    name: "마이다스IT",
+    flex_time: "SELECT",
+    employees_count: 15,
+  });
+  const [employList, setEmployList] = useState(EmployList);
+
+  useEffect(() => {
+    axios({
+      url: BASE_URL + "/companys",
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => {
+      setCompany(res.data);
+    });
+    axios({
+      url: BASE_URL + "/employees/work/status/list",
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => {
+      console.log(res.data);
+      setEmployList(res.data);
+    });
+  }, [setEmployList]);
   return (
     <>
       <SideBarDiv>
         <CompanyInfo>
-          <img src="" alt="" />
+          <img src={CompanyLogo} alt="" />
           <CompanyInfoText>
-            <span>마이다스 아이티</span>
+            <span>{company.name}</span>
             <div>
-              <span>중견기업</span>
-              <span>ㅤ</span>
-              <span>직업 10명</span>
+              <span>직원 {company.employees_count}명</span>
             </div>
-            <span>시차 출퇴근 형</span>
+            <span>
+              {company.flex_time === "SELECT"
+                ? "근무시간 선택형"
+                : company.flex_time === "TIME_DIFFERENCE"
+                ? "시차 출퇴근형"
+                : "집약근무형"}
+            </span>
           </CompanyInfoText>
         </CompanyInfo>
         <hr />
         <SideNav>
           <div>근무 일정</div>
-          <div>근무 현황</div>
+          <Link to={"/status"}>
+            <div>
+              <a>근무 현황</a>
+            </div>
+          </Link>
+          <div
+            onClick={() => {
+              dispatch(settingPlan(!plan));
+            }}
+          >
+            계획하기
+          </div>
+          {admin ? (
+            <Employees>
+              {employList.week_plan.map((item) => (
+                <div>{item.user_name}</div>
+              ))}
+            </Employees>
+          ) : (
+            <></>
+          )}
         </SideNav>
       </SideBarDiv>
     </>
@@ -27,6 +92,10 @@ const SideBar = () => {
 };
 
 export default SideBar;
+
+const Employees = styled.div`
+  margin-left: 10px;
+`;
 
 const SideBarDiv = styled.div`
   width: 400px;
@@ -52,6 +121,9 @@ const CompanyInfo = styled.div`
   display: inline-flex;
   gap: 20px;
   margin: 30px 30px 55px 30px;
+  img {
+    border-radius: 50%;
+  }
 `;
 const CompanyInfoText = styled.div`
   display: flex;
@@ -69,6 +141,7 @@ const CompanyInfoText = styled.div`
 const SideNav = styled.div`
   padding: 80px 50px;
   div {
+    cursor: pointer;
     font-size: 18px;
     margin-bottom: 15px;
   }
